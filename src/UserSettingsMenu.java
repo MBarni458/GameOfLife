d66dd426d0a880b6f416f46eea57e0cfc8e90355
MüllerBarni <mbarni458@gmail.com>
2023-10-16 20:33:07 +0200

    Fix warnings

--- src/UserSettingsMenu.java
+++ src/UserSettingsMenu.java
@@ -1,20 +1,14 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 
 public class UserSettingsMenu extends JPanel {
     public UserSettingsMenu() {
         JButton startButton = new JButton("Start");
         JSlider slider = new JSlider(JSlider.HORIZONTAL, 0, 50, 25);
 
-        startButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                UserConfiguration.activeSimulation=!UserConfiguration.activeSimulation;
-                System.out.println(UserConfiguration.activeSimulation);
-                startButton.setText((startButton.getText()=="Start")?"Stop":"Start");
-            }
+        startButton.addActionListener(e -> {
+            UserConfiguration.activeSimulation=!UserConfiguration.activeSimulation;
+            System.out.println(UserConfiguration.activeSimulation);
+            startButton.setText((startButton.getText().equals("Start"))?"Stop":"Start");
         });
 
         // Add the button and slider to the panel
